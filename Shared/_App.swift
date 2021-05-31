d56517990990498d331262887035aa9d06d04aaa
eric <jinchenyang2005@icloud.com>
2021-05-31 09:55:19 +0800

    Initial Commit

---
+++ Shared/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  Shared
+//
+//  Created by ERIC JIN on 2021/5/31.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
